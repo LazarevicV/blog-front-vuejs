4ae205daabd22ee4973e6603baba5a8b4a83cea6
LazarevicV <vukadinlazarevic@gmail.com>
2023-09-22 02:01:09 +0200

    added page for making a blog post

--- src/views/BlogAddView.vue
+++ src/views/BlogAddView.vue
@@ -44,15 +44,29 @@
 
       <button
         type="submit"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        class="relative bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Add Blog
+        <div v-if="loading" class="loader"></div>
       </button>
     </form>
+
+    <div v-if="successMessage" class="mt-4 rounded overflow-hidden">
+      <div
+        class="bg-green-200 text-green-800 p-4 flex justify-between items-center transition duration-1000"
+      >
+        <div>{{ successMessage }}</div>
+        <button @click="closeNotification" class="text-gray-700">
+          &times;
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -61,14 +75,64 @@ export default {
         content: '',
         image: '',
       },
+      successMessage: '',
+      loading: false,
     }
   },
   methods: {
-    addBlog() {
-      console.log('New Blog Data:', this.blog)
+    async addBlog() {
+      if (
+        this.blog.title === '' ||
+        this.blog.content === '' ||
+        this.blog.image === ''
+      ) {
+        return
+      }
+      this.loading = true // Show the loader
+      try {
+        // Simulate an API call delay of half a second
+        await new Promise((resolve) => setTimeout(resolve, 500))
+        this.successMessage = 'Blog added successfully!'
+        // Automatically close the notification after 5 seconds
+        setTimeout(() => {
+          this.successMessage = ''
+        }, 5000)
+      } catch (error) {
+        console.error('Error adding the blog:', error)
+      } finally {
+        this.loading = false // Hide the loader
+      }
+    },
+    closeNotification() {
+      this.successMessage = ''
     },
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+/* Your styles for the BlogAdd component */
+
+.loader {
+  margin-left: 20vh;
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  animation: spin 0.5s linear infinite;
+  position: absolute;
+  top: 45%;
+  left: 55%; /* Adjusted left value */
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
